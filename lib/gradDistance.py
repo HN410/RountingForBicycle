110b753268170418c1c75347d532964d7723890b
HN410 <HN410>
2022-03-23 16:40:01 +0900

    add addGradDisToGraph

--- lib/gradDistance.py
+++ lib/gradDistance.py
@@ -1,4 +1,4 @@
-from elevation import ElevationGetter
+from .elevation import ElevationGetter
 import json 
 import numpy as np 
 import pandas as pd 
@@ -18,22 +18,24 @@ R = 6370
 L_D = 2 * np.pi * R / 360 * 1000 # 1度当たりの距離(m)
 L_D * 0.0002 # 20m くらい
 # 2e-4度の差が大体20m，この幅に区切って傾斜を調べる
-dist_threshold = 20 # 20m間隔で傾斜を調べる
-grad_coef = 10000
+DIST_THRESHOLD = 20 # 20m間隔で傾斜を調べる
+GRAD_COEF = 10000
+DEFAULT_COLUMN_NAME = "GradDist"
 
 # 道路グラフに傾斜を考慮した距離の欄を追加する
 class GradDistance():
     # hate_coef ... 傾斜をどれだけ避けるか
-    def __init__(self, hate_coef = 1):
+    def __init__(self, hate_coef = 1, column_name = DEFAULT_COLUMN_NAME):
         self.elevation_getter = ElevationGetter()
-        self.l_d = hate_coef * grad_coef
+        self.l_d = hate_coef * GRAD_COEF
+        self.column_name = column_name
 
     # 無向グラフだと仮定して，傾斜があったらすべて値を大きくする
     def calcGradDisUnit(self, x0, y0, x1, y1, dist):
         ele0 = self.elevation_getter.getElevation([y0, x0]) # 緯度が先なので注意
         ele1 = self.elevation_getter.getElevation([y1, x1])
         grad = np.abs(ele0 - ele1) / dist
-        return dist * (1 + grad_coef * (grad ** 2))
+        return dist * (1 + GRAD_COEF * (grad ** 2))
 
 
     # 傾斜も考慮に入れた距離を算出
@@ -42,9 +44,9 @@ class GradDistance():
         dist = np.sqrt((x0-x1) ** 2 + (y0 - y1) ** 2)
         dist *= self.l_d
 
-        if(dist > dist_threshold):
+        if(dist > DIST_THRESHOLD):
             # 一定以上の長さなら，分割して高度を計算
-            divider = int(dist // dist_threshold) + 1
+            divider = int(dist // DIST_THRESHOLD) + 1
             dist_unit = dist / divider
             x_unit, y_unit = (x1-x0)/divider , (y1 - y0) / divider
             return np.array([self.calcGradDisUnit(x0 + x_unit*i, y0 + y_unit*i, x0 + x_unit*(i+1) , y0 + y_unit * (i+1), dist_unit) for i in range(divider)]).sum()
@@ -57,5 +59,11 @@ class GradDistance():
         n = len(x)
         return np.array([self.calcGradDistance(x[i], y[i], x[i + 1], y[i+1])  for i in range(n - 1)]).sum()
 
-    
-        
+
+    # グラフにGradDisの項を追加する
+    def addGradDisToGraph(self, G):
+        gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+        geometryArray = gdf_edges["geometry"].to_numpy()
+        gdf_edges[self.column_name] = [self.lineStringToGradDist(geo) for geo in geometryArray]
+        graph_attrs = {'crs': 'epsg:4326', 'simplified': True}
+        return ox.graph_from_gdfs(gdf_nodes, gdf_edges, graph_attrs)
